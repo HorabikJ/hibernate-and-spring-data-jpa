9cd45dad03c530f2a5c5f006bec7728f0417e0d0
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2024-05-15 21:13:23 +0200

    Adding schema.sql file to properties - knowledge.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,6 +9,9 @@ create table book (
                       primary key (id)
 ) engine=InnoDB;
 
+-- In Hibernate 5 and below there was one table `hibernate_sequence` that was used by all tables in db.
+-- From Hibernate 6, each entity table has its own sequence table, which is much better from performance point of 
+-- view. The sequence table name convention goes like this: <table-name>_seq, that for `book` table gives `book_seq`.  
 create table book_seq (
     next_val bigint
 ) engine=InnoDB;
