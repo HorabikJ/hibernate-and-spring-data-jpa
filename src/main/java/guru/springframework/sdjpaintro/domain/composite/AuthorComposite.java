605a2534eaa27aca0c9e2cdbfc1dc1dfab47c5d6
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2024-06-03 16:49:25 +0200

    Add AuthorComposite entity with composite PK.

---
+++ src/main/java/guru/springframework/sdjpaintro/domain/composite/AuthorComposite.java
@@ -0,0 +1,45 @@
+package guru.springframework.sdjpaintro.domain.composite;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+
+/**
+ * In {@code AuthorComposite} class we use composite PK. We need to specify the class that is an abstraction for
+ * composite PK by the {@code @IdClass} annotation.
+ */
+@Entity
+@IdClass(NameId.class)
+public class AuthorComposite {
+
+    @Id
+    private String firstName;
+
+    @Id
+    private String lastName;
+    private String country;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
