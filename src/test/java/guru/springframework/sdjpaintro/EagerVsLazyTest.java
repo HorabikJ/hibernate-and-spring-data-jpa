9e673e096fcd4dcf15f1d5e4739e00a5933ac821
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2024-06-08 19:52:21 +0200

    Comment to eager vs lazily fetched entities.

--- src/test/java/guru/springframework/sdjpaintro/EagerVsLazyTest.java
+++ src/test/java/guru/springframework/sdjpaintro/EagerVsLazyTest.java
@@ -6,6 +6,7 @@ import guru.springframework.sdjpaintro.hibernateJavaMappings.repository.Customer
 import guru.springframework.sdjpaintro.hibernateJavaMappings.repository.OrderHeaderRepository;
 import guru.springframework.sdjpaintro.hibernateJavaMappings.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -15,14 +16,13 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @ActiveProfiles("local")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Slf4j
-public class ExperimentalTest {
+public class EagerVsLazyTest {
 
     @Autowired
     private CustomerRepository customerRepository;
@@ -37,6 +37,7 @@ public class ExperimentalTest {
 
     @Test
     @Commit
+    @Order(1)
     public void setupTestData() {
         // Products inserted via flyway script, I am using those because when I tried to insert products and 
         // categories inside of the test it did not work. 
@@ -46,7 +47,7 @@ public class ExperimentalTest {
         Product product1 = productRepository.getByDescription("PRODUCT1");
         Product product2 = productRepository.getByDescription("PRODUCT2");
         List<OrderHeader> orderHeaders = new LinkedList<>();
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 50; i++) {
             Address adr = new Address("address" + i, "city" + i, "state" + i, "zipCode" + i);
             Address billingAdr = new Address("billing_address" + i, "billing_city" + i, "billing_state" + i, "billing_zipCode" + i);
             Address shippingAdr = new Address("shipping_address" + i, "shipping_city" + i, "shipping_state" + i, "shipping_zipCode" + i);
@@ -66,9 +67,22 @@ public class ExperimentalTest {
     }
 
     @Test
-    public void test() {
-        Optional<OrderHeader> orderHeader = orderHeaderRepository.findById(IDS.get(51));
-        System.out.println("lalala");
+    @Order(2)
+    public void eagerVsLazyTest() {
+        // All @..ToOne relations are by default eagerly fetched. Look at the below findById sql query in the logs. 
+        // Customer and OrderHeader tables are left joined by their foreign keys to OrderHeader table, that is the 
+        // result of FetchType.EAGER. All eagerly fetched entities are fetched together in one query with the main, 
+        // root entity.
+        // All @..ToMany relations are lazily fetched, select sql for them are executed when a reference to such 
+        // lazily fetched entities is used in a code. Interesting is that select sql is executed when code calls 
+        // `orderLines.size()` not when code calls `orderHeader.getOrderLines()` it means that we have to make the 
+        // use of a reference to lazily fetched entities for the sql to be executed.
+        // Comment out the `orderLines.size()` and the select sql will not be executed.
+        log.info("before select order header query");
+        OrderHeader orderHeader = orderHeaderRepository.findById(IDS.get(IDS.size() / 2)).get();
+        log.info("before orderHeader.getOrderLines - order lines are fetched lazily");
+        Set<OrderLine> orderLines = orderHeader.getOrderLines();
+        log.info("Size of order lines is: " + orderLines.size());
     }
 
 }
